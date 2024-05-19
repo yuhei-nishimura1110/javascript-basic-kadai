b821d0e6f65998d006b689820ee8578c14814fbf
yuhei.nishimura.2005 <yuhei.nishimura,2005@gmail.com>
2024-05-19 11:02:59 +0900

    js2

---
+++ 課題js2/calc.js
@@ -0,0 +1,19 @@
+//numの宣言代入
+let num = Math.floor(Math.random() * 99)+1;
+
+//条件分岐
+if(num % 3 === 0 && num % 5 !== 0 ){
+    console.log('3の倍数です')
+};
+
+if(num % 3 !== 0 && num % 5 === 0 ){
+    console.log('5の倍数です')
+};
+
+if(num % 3 === 0 && num % 5 === 0 ){
+    console.log('3と5の倍数です')
+};
+
+if(num % 3 !== 0 && num % 5 !== 0 ){
+    console.log(num)
+};
