5a11950986648369bfd0ca39938526a301f8cc58
yuhei.nishimura.2005 <yuhei.nishimura,2005@gmail.com>
2024-05-21 14:10:10 +0900

    js6

---
+++ 課題js6/event.js
@@ -0,0 +1,10 @@
+//定義
+const TEXT = document.getElementById('text');
+
+const BTN = document.getElementById('btn');
+
+BTN.addEventListener('click', () => {
+    TEXT.textContent = 'ボタンをクリックしました';
+
+    
+});
